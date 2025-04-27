71aa7d03272c218a40094f92b08703e8fd720069
Andrija <akinovak@gmail.com>
2025-04-27 23:07:49 +0200

    fast mtree with inplace sha

--- MerkleTree/src/MerkleTree.jl
+++ MerkleTree/src/MerkleTree.jl
@@ -1,95 +1,178 @@
 module MerkleTree
 
-using SHA
+using SHA, BCCSHA
 import Base: sizeof
 
 export build_merkle_tree, get_root, get_depth, build_merkle_tree_fast
 export CompleteMerkleTree, MerkleRoot, BatchedMerkleProof
 export prove, verify
 
-function threaded_each(nt::Int, xs::AbstractVector, f::Function; threshold::Int=256)
-    n = length(xs)
+# function threaded_each(nt::Int, xs::AbstractVector, f::Function; threshold::Int=256)
+#     n = length(xs)
 
-    if n <= threshold || nt == 1
-        @inbounds for i in 1:n
-            f(xs, i)
-        end
-        return
-    end
+#     if n <= threshold || nt == 1
+#         @inbounds for i in 1:n
+#             f(xs, i)
+#         end
+#         return
+#     end
 
-    chunk_size = ceil(Int, n / nt)
-    Threads.@sync for t in 1:nt
-        Threads.@spawn begin
-            start_idx = (t - 1) * chunk_size + 1
-            end_idx = min(t * chunk_size, n)
+#     chunk_size = ceil(Int, n / nt)
+#     Threads.@sync for t in 1:nt
+#         Threads.@spawn begin
+#             start_idx = (t - 1) * chunk_size + 1
+#             end_idx = min(t * chunk_size, n)
 
-            @inbounds for i in start_idx:end_idx
-                f(xs, i)
-            end
-        end
-    end
-end
+#             @inbounds for i in start_idx:end_idx
+#                 f(xs, i)
+#             end
+#         end
+#     end
+# end
 
 # for now simply assume that isbitstype(T) = true
-hash_leaf(leaf) = sha256(reinterpret(UInt8, leaf))
-hash_siblings(left_hash::Vector{UInt8}, right_hash::Vector{UInt8}) = sha256(vcat(left_hash, right_hash))
+hash_leaf(leaf) = SHA.sha256(reinterpret(UInt8, leaf))
+hash_siblings(left_hash::Vector{UInt8}, right_hash::Vector{UInt8}) = SHA.sha256(vcat(left_hash, right_hash))
 
 function is_power_of_two(n::Int)
     return n > 0 && (n & (n - 1)) == 0
 end
 
 struct CompleteMerkleTree
-    tree::Vector{Vector{Vector{UInt8}}}
+    tree::Vector{Vector{UInt8}}  # Each layer is flat 32*n bytes
 end
 
-function build_merkle_tree_fast(leaves::AbstractVector)
+function reset_ctx!(ctx::BCCSHA.SHA256_CTX)
+    copyto!(ctx.state, BCCSHA.SHA2_256_initial_hash_value)
+    ctx.bytecount = 0
+    fill!(ctx.buffer, 0x00)
+    ctx.used = false
+end
+
+function hash_array(x::AbstractArray)
+    n = length(x)
+    xh = Vector{UInt8}(undef, 32 * n)
+
+    xh_ptr = pointer(xh)
+
+    nt = Threads.nthreads() 
+    chunk_size = ceil(Int, n / nt)
+    Threads.@sync for t in 1:nt
+        Threads.@spawn begin
+            ctx = BCCSHA.SHA256_CTX()
+            start_idx = (t - 1) * chunk_size + 1
+            end_idx = min(t * chunk_size, n)
+
+            @inbounds for i in start_idx:end_idx
+                BCCSHA.update!(ctx, reinterpret(UInt8, x[i]))
+                BCCSHA.digest_inplace!(xh_ptr + (i-1)*32, ctx)
+                reset_ctx!(ctx)
+            end
+        end
+    end
+
+    return xh
+end
+
+function build_merkle_tree_fast(leaves::AbstractArray)
     @assert is_power_of_two(length(leaves))
     if isempty(leaves)
         return []
     end
 
-    nt = Threads.nthreads()
     n = length(leaves)
+    lh = hash_array(leaves)
+    layers = [lh]
 
-    layer0 = Vector{Vector{UInt8}}(undef, n)
-    f = (curr, i) -> curr[i] = hash_leaf(leaves[i])
-    threaded_each(nt, layer0, f)
+    current_layer = lh
+    current_n = n
 
-    tree = [layer0]
+    while current_n > 1
+        next_n = (current_n + 1) ÷ 2  # ceil div by 2
+        next_layer = Vector{UInt8}(undef, 32 * next_n)
+        next_ptr = pointer(next_layer)
+        curr_ptr = pointer(current_layer)
 
-    while length(tree[end]) > 1
-        parent_layer = tree[end]
-        parent_len = length(parent_layer)
-        child_len = parent_len ÷ 2
-        child_layer = Vector{Vector{UInt8}}(undef, child_len)
+        nt = Threads.nthreads()
+        chunk_size = ceil(Int, next_n / nt)
 
-        if child_len <= 64 || nt == 1
-            for i in 1:child_len
-                left = parent_layer[2i - 1]
-                right = parent_layer[2i]
-                child_layer[i] = hash_siblings(left, right)
-            end
-        else 
-            chunk_size = ceil(Int, child_len / nt)
-            Threads.@sync for t in 1:nt
-                Threads.@spawn begin
-                    start_idx = (t - 1) * chunk_size + 1
-                    end_idx = min(t * chunk_size, child_len)
-        
-                    @inbounds for i in start_idx:end_idx
-                        left = parent_layer[2i - 1]
-                        right = parent_layer[2i]
-                        child_layer[i] = hash_siblings(left, right)
-                    end
+        Threads.@sync for t in 1:nt
+            Threads.@spawn begin
+                ctx = BCCSHA.SHA256_CTX()
+                buf64 = Vector{UInt8}(undef, 64)
+                buf_prt = pointer(buf64)
+
+                start_idx = (t-1)*chunk_size + 1
+                end_idx = min(t*chunk_size, next_n)
+
+                @inbounds for i in start_idx:end_idx
+                    src = curr_ptr + (2*(i-1))*32
+                    unsafe_copyto!(buf_prt, src, 64)
+
+                    BCCSHA.update!(ctx, buf64)
+                    BCCSHA.digest_inplace!(next_ptr + (i-1)*32, ctx)
+                    reset_ctx!(ctx)
                 end
             end
         end
 
-        push!(tree, child_layer)
+        push!(layers, next_layer)
+
+        current_layer = next_layer
+        current_n = next_n
     end
 
-    return CompleteMerkleTree(tree)
+    return CompleteMerkleTree(layers)
 end
+
+# function build_merkle_tree_fast(leaves::AbstractVector)
+#     @assert is_power_of_two(length(leaves))
+#     if isempty(leaves)
+#         return []
+#     end
+
+#     nt = Threads.nthreads()
+#     n = length(leaves)
+
+#     layer0 = Vector{Vector{UInt8}}(undef, n)
+#     f = (curr, i) -> curr[i] = hash_leaf(leaves[i])
+#     threaded_each(nt, layer0, f)
+
+#     tree = [layer0]
+
+#     while length(tree[end]) > 1
+#         parent_layer = tree[end]
+#         parent_len = length(parent_layer)
+#         child_len = parent_len ÷ 2
+#         child_layer = Vector{Vector{UInt8}}(undef, child_len)
+
+#         if child_len <= 64 || nt == 1
+#             for i in 1:child_len
+#                 left = parent_layer[2i - 1]
+#                 right = parent_layer[2i]
+#                 child_layer[i] = hash_siblings(left, right)
+#             end
+#         else 
+#             chunk_size = ceil(Int, child_len / nt)
+#             Threads.@sync for t in 1:nt
+#                 Threads.@spawn begin
+#                     start_idx = (t - 1) * chunk_size + 1
+#                     end_idx = min(t * chunk_size, child_len)
+        
+#                     @inbounds for i in start_idx:end_idx
+#                         left = parent_layer[2i - 1]
+#                         right = parent_layer[2i]
+#                         child_layer[i] = hash_siblings(left, right)
+#                     end
+#                 end
+#             end
+#         end
+
+#         push!(tree, child_layer)
+#     end
+
+#     return CompleteMerkleTree(tree)
+# end
 
 
 function build_merkle_tree(leaves::AbstractVector)
@@ -137,8 +220,17 @@ struct MerkleRoot
 end
 
 sizeof(x::MerkleRoot) = sizeof(x.root)
-get_root(c::CompleteMerkleTree) = MerkleRoot(isempty(c.tree) ? nothing : c.tree[end][1])
+get_root(c::CompleteMerkleTree) = MerkleRoot(isempty(c.tree) ? nothing : c.tree[end])
 get_depth(c::CompleteMerkleTree) = length(c.tree) - 1
+
+struct FlatLayerView
+    data::Vector{UInt8}
+end
+
+function Base.getindex(flv::FlatLayerView, idx::Int)
+    start = (idx - 1) * 32 + 1
+    return @view flv.data[start:start+31]
+end
 
 function ith_layer!(current_layer, queries_len, queries, proof)
     next_queries_len = 0
@@ -187,7 +279,8 @@ function prove(tree::CompleteMerkleTree, queries)
     queries_buff .-= 1
     queries_cnt = length(queries)
     for i in 1:depth
-        queries_cnt = ith_layer!(tree.tree[i], queries_cnt, queries_buff, proof)
+        current_layer = FlatLayerView(tree.tree[i])
+        queries_cnt = ith_layer!(current_layer, queries_cnt, queries_buff, proof)
     end
 
     return BatchedMerkleProof(proof)
