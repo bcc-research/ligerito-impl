f2b8ef973eb8f903d8c10d380eb11e172bf24c8a
Andrija <akinovak@gmail.com>
2026-01-27 12:33:15 -0500

    expander experiments

--- Expander/src/Expander.jl
+++ Expander/src/Expander.jl
@@ -9,14 +9,31 @@ struct ExpanderMatrix{T<:BinaryElem}
 end
 
 function ExpanderMatrix(num_of_constraints::Int, msg_len::Int, d::Int, ::Type{T}) where T <: BinaryElem
-    constraints = Vector{Vector{Tuple{Int, T}}}(undef, num_of_constraints)
+    constraints = [Vector{Tuple{Int, T}}() for _ in 1:num_of_constraints]
+
+    for i in 1:msg_len
+        constraint_indices = sample(1:num_of_constraints, d; replace=false)
+        # constraint_values = rand(T, d)
+        # constraints[i] = [(idx, val) for (idx, val) in zip(constraint_indices, constraint_values)]
+        for idx in constraint_indices
+            val = rand(T)
+            push!(constraints[idx], (i, val))
+        end
+    end
 
     for i in 1:num_of_constraints
-        constraint_indices = sort(sample(1:msg_len, d; replace=false))
-        constraint_values = rand(T, d)
-        constraints[i] = [(idx, val) for (idx, val) in zip(constraint_indices, constraint_values)]
+        constraints[i] = sort(constraints[i]; by=x->x[1])
     end
+
     return ExpanderMatrix{T}(msg_len, constraints)
+end
+
+function avg_w(e::ExpanderMatrix{T}) where T <: BinaryElem
+    total = 0
+    for constraint in e.constraints
+        total += length(constraint)
+    end
+    return total / length(e.constraints)
 end
 
 function encode!(c, expander::ExpanderMatrix{T}) where T <: BinaryElem
@@ -29,8 +46,83 @@ function encode!(c, expander::ExpanderMatrix{T}) where T <: BinaryElem
     end
 end
 
-export ExpanderMatrix, encode!
+function encode_xors!(c, expander::ExpanderMatrix{T}) where T <: BinaryElem
+    for (i, constraint) in enumerate(expander.constraints)
+        acc = zero(T)
+        for (idx, _) in constraint
+            acc += c[idx]
+        end
+        c[expander.msg_len + i] = acc
+    end
+end
 
+function simple(x, indices, y, ::Type{T}) where T <: BinaryElem
+    acc = zero(T)
+    @inbounds for i in eachindex(indices, y)
+        acc += x[indices[i]] * y[i]
+    end
+end
+
+function simple_random_xor(x::Vector{T}, indices::Vector{Int}) where T <: BinaryElem
+    acc = zero(T)
+    @inbounds for i in eachindex(indices)
+        acc += x[indices[i]]
+    end
+end
+
+function simple_xor(x, y, ::Type{T}) where T <: BinaryElem
+    acc = zero(T)
+    @inbounds for i in eachindex(x, y)
+        acc += x[i] + y[i]
+    end
+    return acc
+end
+
+function simple_xor_alone(x, ::Type{T}) where T <: BinaryElem
+    acc = zero(T)
+    @inbounds for i in eachindex(x)
+        acc += x[i]
+    end
+    return acc
+end
+
+function sum_u8_simd(x::Vector{T}) where T <: BinaryElem
+    s = T(0)
+    @inbounds @simd for i in eachindex(x)
+        s += x[i]
+    end
+    return s
+end
+
+function xor_uarrays_simd!(out::Vector{UInt32}, a::Vector{UInt32}, b::Vector{UInt32})
+    # @assert length(a) == 128 && length(b) == 128
+    # @assert length(out) == 128
+    @inbounds for i in 1:128
+        out[i] = a[i] ⊻ b[i]  # same as xor(a[i], b[i])
+    end
+    return out
+end
+
+function emulate_encoding(constraint, data, out)
+    for i in constraint
+        xor_uarrays_simd!(out, out, data[i])
+    end
+end
+
+function emulate_expander(msg_len::Int, d::Int)
+    constraint = sample(1:msg_len, d; replace=false)
+    data = [rand(UInt32, 128) for _ in 1:msg_len]
+
+    return (constraint, data)
+end 
+# a = rand(UInt64, 512)
+# b = rand(UInt64, 512)
+# out = Vector{UInt64}(undef, 512)
+
+# xor_uarrays_simd!(out, a, b)
+
+export ExpanderMatrix, encode!, simple, simple_xor, simple_xor_alone, sum_u8_simd, simple_random_xor
+export encode_xors!, avg_w, xor_uarrays_simd!, emulate_encoding, emulate_expander
 
 # const MIB = 1_048_576 # bytes 
 # const L2_CACHE_SIZE_BYTES_PER_CORE = 3 * MIB
